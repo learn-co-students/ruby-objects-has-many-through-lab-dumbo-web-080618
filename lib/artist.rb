b37ed959711011775397d2faa622636f1588cc12
Elias <eliasiscoding@gmail.com>
2019-10-13 13:28:25 -0400

    video complete

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,39 +1,29 @@
 require 'pry'
 class Artist
   attr_accessor :name
-
   @@all = []
 
   def initialize(name)
     @name = name
-
     @@all << self
-
   end
 
   def self.all
-    @@all
+    @@all 
   end
 
   def new_song(name,genre)
     Song.new(name,self,genre)
-    # binding.pry
   end
 
-  def songs
-    Song.all.select {|song| song.artist == self }
+  def songs 
+    # find all songs that belongs to artist
+    Song.all.select{|song| song.artist == self} 
   end
 
   def genres
-    songs.map do |song|
-      song.genre
-    end
-  end 
-
-
-
-
-
-
+    # finds all the genres that are associated with this artist
+    songs.map {|song| song.genre }
+  end
 
 end
--- lib/song.rb
+++ lib/song.rb
@@ -1,18 +1,26 @@
 class Song
   attr_accessor :name, :artist, :genre
+
   @@all = []
 
-  def initialize(name,artist,genre)
-    @name = name
-    @artist = artist
+  def initialize(name, artist, genre)
+    @name = name 
+    @artist = artist 
     @genre = genre
     @@all << self
   end
 
   def self.all
-    @@all
+    @@all 
   end
 
+  # song belongs to aritst 
+  # song belongs to genre
 
+  # aritst has many songs
+  # genre has many songs
+
+  # artist has many genres through songs
+  # genre has many artist though songs
 
 end
